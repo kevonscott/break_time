d5f0d271e9183b06a0b6fff52aba2c220501401d
kevonscott <kevonscott>
2024-04-20 01:16:27 -0400

    Remove unnecessary src dir and add pillow to requirements.txt

--- tests/test_sound.py
+++ tests/test_sound.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.break_time.utils.util import load_audio_files
+from break_time.utils.util import load_audio_files
 
 
 class TestAudioTestCase(unittest.TestCase):
@@ -8,7 +8,7 @@ class TestAudioTestCase(unittest.TestCase):
         self.audio_assets = load_audio_files()
 
     def test_sound_file_not_found(self):
-        none_existing_file = "sfsfaukjskfihjwwfkas.mp3"
+        none_existing_file = "fake-file.mp3"
         self.assertFalse(none_existing_file in self.audio_assets)
 
     def test_only_mp3(self):
